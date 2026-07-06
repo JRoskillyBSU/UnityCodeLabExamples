82087d4e881d9e5cfed197289cad5764ab56b93a
Joseph Roskilly <j.roskilly@bathspa.ac.uk>
2026-07-06 01:49:58 +0100

    TMP added and minor scene creation

---
+++ Assets/Scripts/UnitConversion.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+
+public class UnitConversion : MonoBehaviour
+{
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        System.Console.WriteLine("Does this work?");
+        print("Things");
+    }
+}
